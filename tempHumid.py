4cef1fa0a4b8283aa9c0d123673c93d5944eab55
AmandaLW <mrs.amandalynnward@gmail.com>
2020-08-20 22:10:41 -0700

    added coversion function to temperature class

--- tempHumid.py
+++ tempHumid.py
@@ -15,17 +15,22 @@ class Humidity:
     def heaterOn(self):
         self.hdc1000.turnHeaterOn()
 
-    def heaterOff(self);
+    def heaterOff(self):
         self.hdc1000.turnHeaterOff()
 
 class Temperature:
     def __init__(self):
         self.hdc1000 = SDL_Pi_HDC1000.SDL_Pi_HDC1000()
 
+    #defalt reading is in celsius
     def getReading(self):
         self.temp = self.hdc1000.readTemperature()
+    #option to get reading in fahrenheit
+    def getReadingF(self):
+        self.tempF = ((self.hdc1000.readTemperature()) * 9/5) + 32
 
 
 temp1 = Temperature()
 temp1.getReading()
-print(temp1.temp)
+temp1.getReadingF()
+print(temp1.tempF)
